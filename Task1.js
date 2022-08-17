769972333401af000511a4d8148ee3609e72224a
manivasagam <s.kishore123.64@gmail.com>
2022-08-17 13:15:47 +0530

    updated

--- Task1.js
+++ Task1.js
@@ -1,18 +1,33 @@
-var obj1 = {
-    name : "person 1",
-    age : 5,
-    }
+// var obj1 = {
+//     name : "person 1",
+//     age : 5,
+//     }
     
-    var obj2 = {
-        age : 5,
-        name : "person 1",
-        }
+//     var obj2 = {
+//         age : 5,
+//         name : "person 1",
+//         }
     
-        m=JSON.stringify(obj1);
-        n=JSON.stringify(obj2);
-        console.log(m);
-        console.log(n);
-        console.log(m===n); //  false
-        console.log(m==n); //  false
+//         m=JSON.stringify(obj1);
+//         n=JSON.stringify(obj2);
+//         console.log(m);
+//         console.log(n);
+//         console.log(m===n); //  false
+//         console.log(m==n); //  false
+
+
+        var obj2 = {
+            age : 5,
+            name : "person 1",
+            }
+        var obj2 = {
+                age : 5,
+                name : "person 1",
+                }
+
+                m=JSON.stringify(obj1);
+                n=JSON.stringify(obj2);
+                console.log(m===n);// true
+
 
 
